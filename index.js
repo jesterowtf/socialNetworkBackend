a5125ec5d2b70438357910eb5afaed77465699b0
Jestero <jesterowtf@gmail.com>
2022-10-03 19:06:37 +0500

    2

--- index.js
+++ index.js
@@ -17,9 +17,11 @@ const app = express();
 
 
 app.use(cors({
-  origin: ["*", "http://localhost:3000", "https://social-network-rho-azure.vercel.app", "social-network-rho-azure.vercel.app"],
+  // origin: ["*", "http://localhost:3000", "https://social-network-rho-azure.vercel.app", "social-network-rho-azure.vercel.app"],
   credentials: true
 }));
+
+app.use(cors());
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
